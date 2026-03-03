25a26666dc4bee933f1605d9304e76790be3e0cb
Miquel <miqc13@gmail.com>
2026-03-03 10:24:01 +0100

    periodic reload added to atom-based computers

--- qcenergy/platforms.py
+++ qcenergy/platforms.py
@@ -230,17 +230,23 @@ class AtomBasedComputer(Computer):
                  t_meas: float = 0.0, 
                  t_clock: float = 0.0,
                  t_transport: float = 0.0,
+                 t_reload: float = 0.0,
+                 t_reload_freq: float = 0.0,
                  N_gatezones: int = 1,
-                 independent_gates: bool = False
+                 independent_gates: bool = False,
+                 periodic_reload: bool = False
                  ):
     
         super().__init__(Nq=Nq, components=components, N_comp=N_comp)
         self.t_reset = t_reset
         self.t_meas = t_meas
         self.t_clock = t_clock
+        self.t_reload = t_reload
+        self.t_reload_freq = t_reload_freq
         self.t_transport = t_transport
         self.N_gatezones = N_gatezones
         self.independent_gates = independent_gates
+        self.periodic_reload = periodic_reload
 
         self.list_components = self.assemble()
 
@@ -261,7 +267,11 @@ class AtomBasedComputer(Computer):
     
     def t_comp(self, D0: int, alpha: float, beta: float, N_gates: int, N_samples: int) -> float:
 
-        return (self.t_reset + self.final_circuit_depth(D0, alpha, N_gates)*self.t_clock + self.t_meas + self.t_transport*self.final_circuit_depth(D0, alpha, N_gates)*beta)*N_samples
+        if not self.periodic_reload:
+            return (self.t_reset + self.final_circuit_depth(D0, alpha, N_gates)*self.t_clock + self.t_meas + self.t_transport*self.final_circuit_depth(D0, alpha, N_gates)*beta)*N_samples
+        else:
+            t_factor = (self.t_reload_freq + self.t_reload) / self.t_reload_freq
+            return (self.t_reset + self.final_circuit_depth(D0, alpha, N_gates)*self.t_clock + self.t_meas + self.t_transport*self.final_circuit_depth(D0, alpha, N_gates)*beta)*N_samples*t_factor
     
     def N_pi(self, t: float, D0: int, alpha: float, beta: float, N_gates: int, N_samples: int) -> float:
         
